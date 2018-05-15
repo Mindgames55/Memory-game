a02325b5b4e08ab36479a53c762025effc7dc405
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-05-15 00:53:49 -0400

    changing img extension

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-document.loaded(function(){
+document.addEventListener('DOMContentLoaded', function(){
   const fragment=document.createDocumentFragment();
   for (let i=1;i<=16;i++){
-      let nameJpg=(i>8)?i-8;i;
+      let nameJpg=(i>8)?i-8:i;
       const card=document.createElement('img');
-      card.setAttribute('src','img/'+nameJpg+'jpg');
+      card.setAttribute('src','img/svg/'+nameJpg+'.svg');
       fragment.appendChild(card);
   }
-  const grid=getElementsByClassName('grid');
-  grid.appendChild(fragment);
-})
+  const cardGrid=document.getElementsByClassName('grid');
+  cardGrid[0].appendChild(fragment);
+});
