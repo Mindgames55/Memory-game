c6581feb768b563a51af3deba8b31329128384d4
Mayguen Ojeda <mayguen.ojeda88@gmail.com>
2018-05-15 21:37:01 -0400

    add random img

--- js/app.js
+++ js/app.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function(){
+  // this function creates and array with couples of random numbers from 1 to limSup.
+  function sorting(limSup){
+    let array=[];
+    for (let i=1;i<=2*limSup;i++){
+        let value=(i>limSup)?i-limSup:i;
+        array.push(value);
+    }
+    return array.sort(function(a, b){return 0.5 - Math.random()});
+  }
+
   const fragment=document.createDocumentFragment();
-  for (let i=1;i<=16;i++){
-      let nameJpg=(i>8)?i-8:i;
+  let nameJpg=sorting(8);
+  for (let i=0;i<16;i++){
       const card=document.createElement('button');
-      //card.setAttribute('src','img/svg/'+nameJpg+'.svg');
-      HTMLButtonElement.tyoe='button';
+      HTMLButtonElement.type='button';
       card.className='card';
+      const cardImg=document.createElement('img');
+      cardImg.setAttribute('src','img/svg/'+nameJpg[i]+'.svg');
+      card.appendChild(cardImg);
       fragment.appendChild(card);
   }
   const cardGrid=document.getElementsByClassName('grid');
   cardGrid[0].appendChild(fragment);
+
 });
